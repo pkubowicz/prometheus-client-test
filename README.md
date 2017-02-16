2b771005d6a30e4532e3fb0c04e86a4021f7a6f3
Piotr Kubowicz <piotr.kubowicz@gmail.com>
2017-02-16 14:26:36 +0100

    Rewrite
    - don't use DefaultExports collectors, as they
      change in time
      use a set of collectors that return the same
      all the time
    - add more variety of data: escaped label values and help
      text, many label values
    - warm up each implementation to allow JVM to
      generate machine code
      previously only 'PerformantTextFormat' was warmed up
    - force GC between runs to allow measuring memory performance
    - use a standard Gradle distribution

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # prometheus-client-test
+
+Performance test of Prometheus Java client text formatting.
+
+## Measure time
+
+    ./gradlew shadowJar && java -jar build/libs/prometheus-client-test-all.jar
+
+## Measure memory efficiency
+
+    ./gradlew shadowJar && java -XX:+PrintGC -jar build/libs/prometheus-client-test-all.jar PerformantTextFormat
+
+Manually copy GC output between lines `Starting` and `Finished` to a GC analyser like [GCViewer](https://github.com/chewiebug/GCViewer).
--- src/main/java/com/workday/prometheus/test/Main.java
+++ src/main/java/com/workday/prometheus/test/Main.java
@@ -1,118 +1,159 @@
 package com.workday.prometheus.test;
 
+import io.prometheus.client.CollectorRegistry;
+import io.prometheus.client.Counter;
+import io.prometheus.client.Gauge;
+import io.prometheus.client.exporter.common.TextFormat;
+
 import java.io.IOException;
 import java.io.StringWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-
-import io.prometheus.client.Collector;
-import io.prometheus.client.CollectorRegistry;
-import io.prometheus.client.Gauge;
-import io.prometheus.client.Gauge.Timer;
-import io.prometheus.client.exporter.common.TextFormat;
-import io.prometheus.client.hotspot.DefaultExports;
+import java.util.function.BiConsumer;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Main {
-  private static final int ITERATIONS = 10000;
+  private static final int WARM_UP_ITERATIONS = 2_000;
+  private static final int ITERATIONS = 10_000;
+
+  private static class Implementation {
+    private final String description;
+    private final BiConsumer<StringWriter, CollectorRegistry> formatImplementation;
+
+    Implementation(String description, BiConsumer<StringWriter, CollectorRegistry> formatImplementation) {
+      this.description = description;
+      this.formatImplementation = formatImplementation;
+    }
+  }
+
+  private static final List<Implementation> IMPLEMENTATIONS = Arrays.asList(
+      new Implementation("Performant2TextFormat", Main::runPerformant2TextFormat),
+      new Implementation("PerformantTextFormat", Main::runPerformantTextFormat),
+      new Implementation("InterpolationTextFormat", Main::runInterpolationTextFormat),
+      new Implementation("StringBuilderTextFormat", Main::runStringBuilderTextFormat),
+      new Implementation("LegacyTextFormat", Main::runLegacyTextFormat)
+  );
 
   public static void main(String[] args) throws IOException {
-    DefaultExports.initialize();
+    populateCollectorRegistry();
+    String result = runOnce(IMPLEMENTATIONS.get(0));
+    System.out.printf("%d warm-up iterations, %d iterators with metrics string of length %d\n",
+        WARM_UP_ITERATIONS, ITERATIONS, result.length());
 
-    // warmup run
-    runPerformantTextFormat();
-    Gauge throwawayGauge = new Gauge.Builder().name("throwaway").help("time performant code").create();
-    Timer throwawayTimer = throwawayGauge.startTimer();
-    for (int i = 0; i < ITERATIONS; i++) {
-      runPerformant2TextFormat();
+    List<Implementation> implementations = getImplementations(args);
+
+    System.out.println("Warming up");
+    for (int i = 0; i < WARM_UP_ITERATIONS; i++) {
+      implementations.forEach(Main::runOnce);
     }
-    throwawayTimer.close();
+    System.gc();
+    System.out.println("Warmed up");
 
-    // test new 'performant2' code
-    runPerformantTextFormat();
-    Gauge perf2Gauge = new Gauge.Builder().name("performant2").help("time performant2 code").create();
-    Timer perf2Timer = perf2Gauge.startTimer();
+    implementations.forEach(Main::runAndReport);
+  }
+
+  private static void populateCollectorRegistry() {
+    IntStream.rangeClosed(1, 100).forEach(i ->
+        new Counter.Builder().name("simple_counter" + i).help("short help")
+            .register().inc(i)
+    );
+    IntStream.rangeClosed(1, 40).forEach(i ->
+        new Counter.Builder().name("long_help_counter" + i).help("this help is a bit longer than help text for other elements")
+            .register().inc(i * 100)
+    );
+    IntStream.rangeClosed(1, 20).forEach(i -> {
+      Gauge gauge = new Gauge.Builder().name("throwaway" + i).help("long\\help\nwith\\escaping").register();
+      gauge.set(1.0d / 3);
+      Counter counter = new Counter.Builder().name("with_escaped" + i).help("long\\help\nwith\\escaping")
+          .labelNames("escaped_label")
+          .register();
+      counter.labels("slash\\quote\\newline\nlabel1").inc();
+      counter.labels("slash\\quote\\newline\nlabel2").inc();
+      Counter manyLabelsCounter = new Counter.Builder().name("many_labels" + i).help("short help")
+          .labelNames("l1")
+          .register();
+      IntStream.rangeClosed(1, 15).forEach(j -> manyLabelsCounter.labels("lv" + j).inc(j));
+    });
+  }
+
+  private static void runAndReport(Implementation implementation) {
+    runOnce(implementation);
+    System.out.printf("   Starting %25s\n", implementation.description);
+    long startTime = System.currentTimeMillis();
     for (int i = 0; i < ITERATIONS; i++) {
-      runPerformant2TextFormat();
+      runOnce(implementation);
     }
-    perf2Timer.close();
-    printStats(perf2Gauge.collect());
+    long durationMillis = System.currentTimeMillis() - startTime;
+    System.gc();
+    System.out.printf("   Finished %25s in %5d ms\n", implementation.description, durationMillis);
+  }
 
-    // test new 'performant' code
-    runPerformantTextFormat();
-    Gauge perfGauge = new Gauge.Builder().name("performant").help("time performant code").create();
-    Timer perfTimer = perfGauge.startTimer();
-    for (int i = 0; i < ITERATIONS; i++) {
-      runPerformantTextFormat();
+  private static String runOnce(Implementation implementation) {
+    try (StringWriter sw = new StringWriter(10240)) {
+      implementation.formatImplementation.accept(sw, CollectorRegistry.defaultRegistry);
+      return sw.toString();
+    } catch (IOException e) {
+      throw new RuntimeException(e);
     }
-    perfTimer.close();
-    printStats(perfGauge.collect());
+  }
 
-    // test new 'interpolation' code
-    runInterpolationTextFormat();
-    Gauge interGauge = new Gauge.Builder().name("interpolation").help("time interpolation code").create();
-    Timer interTimer = interGauge.startTimer();
-    for (int i = 0; i < ITERATIONS; i++) {
-      runInterpolationTextFormat();
+  private static void runLegacyTextFormat(StringWriter sw, CollectorRegistry registry) {
+    try {
+      TextFormat.write004(sw, registry.metricFamilySamples());
+    } catch (IOException e) {
+      throw new RuntimeException(e);
     }
-    interTimer.close();
-    printStats(interGauge.collect());
+  }
 
-    // test new 'stringbuilder' code
-    runStringBuilderTextFormat();
-    Gauge sbGauge = new Gauge.Builder().name("stringbuilder").help("time stringbuilder code").create();
-    Timer sbTimer = sbGauge.startTimer();
-    for (int i = 0; i < ITERATIONS; i++) {
-      runStringBuilderTextFormat();
+  private static void runPerformantTextFormat(StringWriter sw, CollectorRegistry registry) {
+    try {
+      PerformantTextFormat.write004(sw, registry.metricFamilySamples());
+    } catch (IOException e) {
+      throw new RuntimeException(e);
     }
-    sbTimer.close();
-    printStats(sbGauge.collect());
+  }
 
-    // test 0.0.19 release code
-    runLegacyTextFormat();
-    Gauge legacyGauge = new Gauge.Builder().name("legacy").help("time legacy code").create();
-    Timer legacyTimer = legacyGauge.startTimer();
-    for (int i = 0; i < ITERATIONS; i++) {
-      runLegacyTextFormat();
+  private static void runPerformant2TextFormat(StringWriter sw, CollectorRegistry registry) {
+    try {
+      PerformantTextFormat2.write004(sw, registry.metricFamilySamples());
+    } catch (IOException e) {
+      throw new RuntimeException(e);
     }
-    legacyTimer.close();
-    printStats(legacyGauge.collect());
   }
 
-  static void runLegacyTextFormat() throws IOException {
-    StringWriter sw = new StringWriter(10240);
-    TextFormat.write004(sw, CollectorRegistry.defaultRegistry.metricFamilySamples());
-    sw.close();
+  private static void runInterpolationTextFormat(StringWriter sw, CollectorRegistry registry) {
+    try {
+      InterpolationTextFormat.write004(sw, registry.metricFamilySamples());
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
   }
 
-  static void runPerformantTextFormat() throws IOException {
-    StringWriter sw = new StringWriter(10240);
-    PerformantTextFormat.write004(sw, CollectorRegistry.defaultRegistry.metricFamilySamples());
-    sw.close();
+  private static void runStringBuilderTextFormat(StringWriter sw, CollectorRegistry registry) {
+    try {
+      StringBuilderTextFormat.write004(sw, registry.metricFamilySamples());
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
   }
 
-  static void runPerformant2TextFormat() throws IOException {
-    StringWriter sw = new StringWriter(10240);
-    PerformantTextFormat2.write004(sw, CollectorRegistry.defaultRegistry.metricFamilySamples());
-    sw.close();
+  private static List<Implementation> getImplementations(String[] args) {
+    final List<Implementation> implementations;
+    if (args.length > 0) {
+      implementations = IMPLEMENTATIONS.stream().filter(implementation -> implementation.description.equals(args[0]))
+          .collect(Collectors.toList());
+      if (implementations.isEmpty()) {
+        System.err.println("Unknown implementation: " + args[0] + "; pass any of " +
+            IMPLEMENTATIONS.stream().map(i -> i.description).sorted().collect(Collectors.joining(",")));
+        System.exit(1);
+      }
+    } else {
+      implementations = new ArrayList<>(IMPLEMENTATIONS);
+      Collections.shuffle(implementations);
+    }
+    return Collections.unmodifiableList(implementations);
   }
-
-  static void runInterpolationTextFormat() throws IOException {
-    StringWriter sw = new StringWriter(10240);
-    InterpolationTextFormat.write004(sw, CollectorRegistry.defaultRegistry.metricFamilySamples());
-    sw.close();
-  }
-
-  static void runStringBuilderTextFormat() throws IOException {
-    StringWriter sw = new StringWriter(10240);
-    StringBuilderTextFormat.write004(sw, CollectorRegistry.defaultRegistry.metricFamilySamples());
-    sw.close();
-  }
-
-  static void printStats(List<Collector.MetricFamilySamples> samples) throws IOException {
-    StringWriter sw = new StringWriter(10240);
-    TextFormat.write004(sw, Collections.enumeration(samples));
-    sw.close();
-    System.out.println(sw.toString());
-  }
-
 }
